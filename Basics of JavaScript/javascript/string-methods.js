28ad01604bf0f54ab1c727ff5841413e5548ef92
VITTHAL KORVAN <korvanvitthal@gmail.com>
2023-03-01 12:17:11 +0530

    updated new programs!

---
+++ Basics of JavaScript/javascript/string-methods.js
@@ -0,0 +1,21 @@
+// trim() 
+// toUpperCase()
+// toLowerCase()
+// slice()
+
+let firstName = "vitthal";
+
+// console.log(firstName.length);
+// firstName = firstName.trim(); // "vitthal"
+// console.log(firstName)
+// console.log(firstName.length);
+// firstName = firstName.toUpperCase();
+// firstName = firstName.toLowerCase();
+// console.log(firstName);
+
+// start index 
+// end index
+
+//if you will not provide end index string automatically stops at the end
+let newString = firstName.slice(0,4); // vitt
+console.log(newString);
---
+++ Basics of JavaScript/javascript/variables.js
@@ -0,0 +1,20 @@
+// use strict is used in javascript for restricting the js code  
+"use strict";
+// intro to variables
+
+// variables can store some information
+// we can use that information later
+// we can change that information later
+
+// declare a variable 
+
+var firstName = "vitthal";
+
+// use a variable 
+console.log(firstName);
+
+// change value 
+
+firstName = "korvan";
+
+console.log(firstName);
