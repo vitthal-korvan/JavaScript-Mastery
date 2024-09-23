94a133b7fe3a35700f39cb2007631059113684c7
Vitthal Korvan <korvanvitthal@gmail.com>
2024-09-23 21:10:19 +0530

    Events Script Updated!

--- JavaScript-Mastery-Part2/Event Bubbling Capturing Delegation/script.js
+++ JavaScript-Mastery-Part2/Event Bubbling Capturing Delegation/script.js
@@ -1,10 +1,22 @@
-// console.log("hello world");
-
-const grandparent = document.querySelector(".grandparent");
+// const grandparent = document.querySelector(".grandparent");
 // const parent = document.querySelector(".parent");
 // const child = document.querySelector(".child");
 
-// capturing events
+//Event Bubbling -> Event propagation
+// child.addEventListener("click", () => {
+//   console.log("bubble child");
+// });
+// parent.addEventListener("click", () => {
+//   console.log("bubble parent");
+// });
+// grandparent.addEventListener("click", () => {
+//   console.log("bubble grandparent");
+// });
+// document.body.addEventListener("click", () => {
+//   console.log("bubble document.body");
+// });
+
+//Event Capturing -> 3rd argument of addEventListener must set to true
 // child.addEventListener(
 //   "click",
 //   () => {
@@ -34,53 +46,7 @@ const grandparent = document.querySelector(".grandparent");
 //   true
 // );
 
-///// not capture
-
-// child.addEventListener("click", () => {
-//   console.log("bubble child");
-// });
-// parent.addEventListener("click", () => {
-//   console.log("bubble parent");
-// });
-// grandparent.addEventListener("click", () => {
-//   console.log("bubble grandparent");
-// });
-// document.body.addEventListener("click", () => {
-//   console.log("bubble document.body");
-// });
-
 // event delegation
 // grandparent.addEventListener("click", (e) => {
 //   console.log(e.target);
 // });
-
-const todoForm = document.querySelector(".form-todo");
-const todoInput = document.querySelector(".form-todo input[type='text']");
-const todoList = document.querySelector(".todo-list");
-
-todoForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const newTodoText = todoInput.value;
-  const newLi = document.createElement("li");
-  const newLiInnerHtml = `
-        <span class="text">${newTodoText}</span>
-        <div class="todo-buttons">
-            <button class="todo-btn done">Done</button>
-            <button class="todo-btn remove">Remove</button>
-        </div>`;
-  newLi.innerHTML = newLiInnerHtml;
-  todoList.append(newLi);
-  todoInput.value = "";
-});
-
-todoList.addEventListener("click", (e) => {
-  // check if user clicked on done button
-  if (e.target.classList.contains("remove")) {
-    const targetedLi = e.target.parentNode.parentNode;
-    targetedLi.remove();
-  }
-  if (e.target.classList.contains("done")) {
-    const liSpan = e.target.parentNode.previousElementSibling;
-    liSpan.style.textDecoration = "line-through";
-  }
-});
